aa3d7622b6fbca590fc135e78134e406e4095274
pey9797 <129020629+pey9797@users.noreply.github.com>
2023-03-29 10:11:39 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -2,9 +2,8 @@
 // First: Local repository modification
 // Second: GitHub modification
 
-
 print("Hello World")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
-
+print("Tell my world")
